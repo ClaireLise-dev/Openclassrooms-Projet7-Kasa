10c004e67f1fa023db77025bcc3dc23d2c977f7f
Claire-Lise <clairedemettre@gmail.com>
2022-12-26 23:15:24 +0100

    Hero Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,13 @@
+import classes from './Home.module.css'
+
 function Home() {
-    return <h1>Accueil</h1>
+    return (
+        <main className={classes.HomeContainer}>
+            <div className={classes.Hero}>
+                <h1>Chez vous, partout et ailleurs.</h1>
+            </div>
+        </main>
+    )
 }
 
 export default Home

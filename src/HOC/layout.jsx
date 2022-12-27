36bffeb65b60fb46d1a1bbb251dc6bc09885ce5f
Claire-Lise <clairedemettre@gmail.com>
2022-12-27 15:20:45 +0100

    Correction footer pleine page

--- src/HOC/layout.jsx
+++ src/HOC/layout.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
+import classes from './layout.module.css'
 
 function Layout(props){
     return (
         <>
+            <div className={classes.GeneralContainer}>
             <Header />
-
-                {props.children}
-
-            <Footer />
+                    {props.children}
+            </div>
+          <Footer />
         </>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect} from 'react'
 import classes from './Home.module.css';
 import houses from '../../data/logements.json';
 import Card from '../../components/card/card';
-import Banner from '../../components/banner/banner'
+import Banner from '../../components/banner/banner';
+import Footer from '../../components/footer/footer'
 
 function Home() {
     const [accomodations, setAccomodations] = useState(houses);
@@ -10,8 +11,9 @@ function Home() {
     useEffect(() =>
         setAccomodations(houses),
     [])
-    
+
     return (
+        
         <main className={classes.HomeContainer}>
             <Banner />
             <section className={classes.Gallery}>
@@ -23,6 +25,7 @@ function Home() {
                 
             </section>
         </main>
+    
     )
 }
 

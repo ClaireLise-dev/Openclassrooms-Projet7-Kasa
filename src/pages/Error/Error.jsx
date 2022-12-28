44aff4c7539851fd451d9d0e56d87301965d1d26
Claire-Lise <clairedemettre@gmail.com>
2022-12-28 08:55:38 +0100

    Mise en page Erreur

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import classes from './Error.module.css';
+import routes from '../../config/routes'
+
 function Error() {
-    return <h1>Page non trouvée</h1>
+    return (
+
+        <main className={classes.ErrorContainer}>
+            <div className={classes.ErrorText}>
+                <h1>404</h1>
+                <p> Oups! La page que vous demandez n'existe pas</p>
+            </div>
+            <div className={classes.Link}>
+                <Link to={routes.HOME}>Retourner à la page d'accueil</Link>
+            </div>
+        </main>
+
+    )
 }
 
 export default Error

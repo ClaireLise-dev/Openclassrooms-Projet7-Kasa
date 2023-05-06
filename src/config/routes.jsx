097ad227146c159b237bd0391dd39ab21bba26e2
Claire-Lise <clairedemettre@gmail.com>
2023-05-06 10:07:37 +0200

    Rectification routes

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -1,7 +1,7 @@
 const routes = {
   HOME: 'Openclassrooms-Projet7-Kasa/',
   ABOUT: 'Openclassrooms-Projet7-Kasa/about',
-  ERROR: 'Openclassrooms-Projet7-Kasa/*',
+  ERROR: '/*',
   ACCOMODATION: 'Openclassrooms-Projet7-Kasa/accomodation/:id',
 }
 
